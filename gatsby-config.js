64c1d6e3f69dcaedd02461563ee370988c3e6951
dimstar <kylermagee@gmail.com>
2018-12-29 14:08:42 -0800

    updates for netlify starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,16 +9,49 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'wpdemo.gatsbycentral.com',
+        baseUrl: 'sandbox.test',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
-        protocol: 'https',
+        protocol: 'http',
         // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: false,
-        auth: {},
+        useACF: true,
+        // auth: {
+        //   // JWT Auth
+        //   jwt_user: process.env.JWT_USER,
+        //   jwt_pass: process.env.JWT_PASSWORD,
+        // },
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 1,
+        // Set WP REST API routes whitelists
+        // and blacklists using glob patterns.
+        // Defaults to whitelist the routes shown
+        // in the example below.
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]`
+        // ` will either include or exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        // Whitelisted routes using glob patterns
+        includedRoutes: [
+          "/*/*/categories",
+          "/*/*/posts",
+          "/*/*/pages",
+          "/*/*/media",
+          "/*/*/tags",
+          "/*/*/taxonomies",
+          "/*/*/users",
+        ],
+        // Blacklisted routes using glob patterns
+        excludedRoutes: [
+          "/*/*/posts/1456",
+          "/*/*/wp-rest-api-log/"
+        ],
         // Set to true to debug endpoints on 'gatsby build'
-        verboseOutput: false,
+        verboseOutput: true,
+        // searchAndReplaceContentUrls: {
+        //   sourceUrl: "http://sandbox.test",
+        //   replacementUrl: "http://api.sandbox.test",
+        // },
       },
     },
     'gatsby-plugin-sharp',
